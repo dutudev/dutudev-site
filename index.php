b6a5ddf33796cfb1162d45be05f56d4e62a3a3c5
dutudev <dutudev@gmail.com>
2026-02-25 20:12:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home - dutu.dev</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contents">
+        <div class="navbar">
+            <div class="homelink">
+                <a href="index.html">dutu.dev</a>
+            </div>
+            <div class="pages">
+                <a href="index.html">Games</a>
+                <a href="index.html">Logs</a>
+                <a href="index.html">About</a>
+            </div>
+        </div>
+        <div class="holder">
+            <p>
+                Welcome to my site, i am dutudev, i like to make games and videos. Here i will have download links for my games and i will make some updates from time to time.
+            </p>
+            <div class="separator"></div>
+            <div class="sectiontitle">
+                <h1>Latest Project</h1>
+
+            </div>
+            <img class="latestProjImg" src="images/LastestProject.png" alt="">
+            <div class="latestProjDetails">
+                <h1>The Entreprenuer</h1>
+                <div class="latestProjLinks">
+                    <a href="https://dutudev.itch.io/the-entrepreneur">itch.io</a>
+                    <a href="add archive here">archive</a>
+                </div>
+            </div>
+        </div> 
+    </div>
+</body>
+</html>
